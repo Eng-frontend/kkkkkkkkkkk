cac617a3da36865acdeda9fe8c3d7837e790b46f
Eng-frontend <114280645+Eng-frontend@users.noreply.github.com>
2022-10-03 10:55:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+let parent_js = document.getElementById("parent");
+let right_js = document.getElementById("right");
+let left_js = document.getElementById("left");
+let center_js = document.getElementById("center");
+let count = 1000;
+window.onload = function(){
+    let set_fun = setInterval(function(){
+     if( count === 5000){
+     right_js.classList.toggle("right");
+     left_js.classList.toggle("left");
+     center_js.classList.toggle("center");
+     clearInterval(set_fun);
+     }
+     else{
+         count+=1000;
+     }
+    },count);
+};
+*/
